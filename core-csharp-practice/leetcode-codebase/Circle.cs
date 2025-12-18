f27d45652d2dad8f788023080db1f026b84e1e6c
practice <aditi.pathak_cs22@gla.ac.in>
2025-12-18 16:29:42 +0530

    [Aditi Pathak] next commit

---
+++ core-csharp-practice/leetcode-codebase/Circle.cs
@@ -0,0 +1,12 @@
+using System;
+class Circle{
+	static void Main()
+	{
+		Console.Write("enter radius");
+		int r=int.Parse(Console.ReadLine());
+		double a=(3.14*r*r);
+		Console.WriteLine(a);
+		
+	}
+}
+		
