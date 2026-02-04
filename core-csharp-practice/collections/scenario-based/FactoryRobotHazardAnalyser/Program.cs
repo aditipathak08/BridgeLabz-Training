3f09c241aac418eee4ef631942de9f66584cf955
practice <aditi.pathak_cs22@gla.ac.in>
2026-02-04 16:26:32 +0530

    [Aditi Pathak]factory

---
+++ core-csharp-practice/collections/scenario-based/FactoryRobotHazardAnalyser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+        double armPrecision=double.Parse(Console.ReadLine());
+        int workerDensity=int.Parse(Console.ReadLine());
+        string machineryState=Console.ReadLine();
+        Analyser ad=new Analyser();
+        double result=ad.CalculateHazardRisk(armPrecision,workerDensity,machineryState);
+        Console.WriteLine("Robot Hazard Risk Score: "+result);
+        }
+        catch(RobotSafetyException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+   }
+}
